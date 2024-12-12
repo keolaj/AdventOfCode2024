32a2667038de4f1fc669e9888825c081cc0f0828
Keola <keolajarvegren@gmail.com>
2024-12-12 01:51:02 -0800

    day 11 part 2

--- days/day11/day11.cpp
+++ days/day11/day11.cpp
@@ -42,7 +42,6 @@ std::pair<uint64_t, uint64_t> transformStone(uint64_t stone, std::unordered_map<
 			return { stone * 2024, UINT64_MAX };
 		}
 	}
-
 }
 
 void blink(std::forward_list<uint64_t>& stones, std::unordered_map<uint64_t, std::pair<uint64_t, uint64_t>>& transformations) {
@@ -53,6 +52,56 @@ void blink(std::forward_list<uint64_t>& stones, std::unordered_map<uint64_t, std
 		if (newStones.second != UINT64_MAX) {
 			stones.insert_after(it, newStones.second);
 			++it;
+		}
+	}
+}
+
+struct PairHash {
+	template <class T1, class T2>
+	std::size_t operator () (const std::pair<T1, T2>& p) const {
+		auto h1 = std::hash<T1>{}(p.first);
+		auto h2 = std::hash<T2>{}(p.second);
+		return std::rotl(h1, 1) ^ h2;
+	}
+};
+
+struct PairEqual {
+	template <class T1, class T2>
+	bool operator () (const std::pair<T1, T2>& p1, const std::pair<T1, T2>& p2) const {
+		return p1.first == p2.first && p1.second == p2.second;
+	}
+};
+
+uint64_t blinkStone(uint64_t stone, std::unordered_map<std::pair<uint64_t, uint64_t>, uint64_t, PairHash, PairEqual>& transform_count, uint64_t count) {
+	if (count == 0) {
+		return 1;
+	}
+	if (stone == 0) {
+		if (transform_count.contains({ stone, count })) return transform_count[{stone, count}];
+		else {
+			auto c = blinkStone(1, transform_count, count - 1);
+			transform_count[{stone, count}] = c;
+			return c;
+		}
+	}
+	else {
+		if (transform_count.contains({ stone, count })) return transform_count[{stone, count}];
+		uint64_t power = 0;
+		for (uint64_t i = stone; i != 0; i /= 10) {
+			++power;
+		}
+		if (power % 2 == 0) {
+			uint64_t higher = stone / (exp(10, power / 2));
+			uint64_t lower = stone - (higher * (exp(10, power / 2)));
+
+			auto c = blinkStone(higher, transform_count, count - 1) + blinkStone(lower, transform_count, count - 1);
+			transform_count[{stone, count}] = c;
+			return c;
+		}
+		else {
+			auto c = blinkStone(stone * 2024, transform_count, count - 1);
+			transform_count[{stone, count}] = c;
+			return c;
 		}
 	}
 }
@@ -71,14 +120,12 @@ void day_main() {
 
 	std::unordered_map<uint64_t, std::pair<uint64_t, uint64_t>> transformations;
 
-	for (uint64_t i = 0; i < 75; ++i) {
-		blink(stones, transformations);
+	std::unordered_map<std::pair<uint64_t, uint64_t>, uint64_t, PairHash, PairEqual> transform_count;
+	
+	uint64_t size = 0;
+	for (auto stone : stones_arr) {
+		size += blinkStone(stone, transform_count, 75);
 	}
 
-	int size = 0;
-	for (auto it = stones.begin(); it != stones.end(); ++it) {
-		++size;
-	}
-
-	printf("RESULT: %d", size);
+	std::cout << size;
 }
